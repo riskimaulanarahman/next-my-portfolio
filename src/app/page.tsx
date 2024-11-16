d1124bd43f81d3e2c12b1083cd82e591c6b7af7c
Riski Maulana Rahman <61986465+riskimaulanarahman@users.noreply.github.com>
2024-11-16 10:27:34 +0800

    remove nav body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,6 @@ export default function Home() {
 			<h1>Hi, I&apos;m Riski Maulana Rahman!</h1>
 			<Image src="/profile.jpeg" alt="Riski's Photo" width={200} height={200} />
 			<p>A passionate Full-Stack Developer.</p>
-			<div>
-				<a href="/about">About Me</a>
-				<a href="/projects">Projects</a>
-				<a href="/contact">Contact</a>
-			</div>
 		</main>
 	);
 }
